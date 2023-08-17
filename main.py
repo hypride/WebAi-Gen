8089bd724c48207869a30f07c8a471706ed1e815
hackerstore <106470800+hackerstore@users.noreply.github.com>
2023-08-17 17:25:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,14 @@
 import os
 import math
-
 import random
 import time
 import pyfiglet
 
 
 
-def datasec():
-	os.system("cd theme/DataSec && python main.py")
-
-
-def colorlib():
-	os.system("cd theme/ColorLib && python main.py")
-	
-	
-
-	
-	
-	
-	
-
+def personal():
+	personal_theme_list = ['os.system("cd theme/DataSec && python main.py")', 'os.system("cd theme/ColorLib && python main.py")']
+	exec(random.choice(personal_theme_list))
 
 
 
@@ -31,20 +19,19 @@ def main():
 	print("==================================================")
 	time.sleep(2)
 	print("Ai: Hello, Select your theme..")
-	print("1] Colorlib 2] DataSec")
-	print("3] Colorlib2 4] DataSec2")
+	print("1] Personal Websites")
+	print("2] Company Websites")
+	print("3] Internet forum")
 	print("----Exit----")
 	input1 = input("Enter Your Option: ")
 	if input1 == "1":
-		colorlib()
+		personal()
 	elif input1 == "2":
-		datasec()
-	elif input1 == "3":
 		print("Not Working... Coming Soon..")
 		print("Return in 5 second")
 		time.sleep(5)
 		main()
-	elif input1 == "4":
+	elif input1 == "3":
 		print("Not Working... Coming Soon..")
 		print("Return in 5 second")
 		time.sleep(5)
